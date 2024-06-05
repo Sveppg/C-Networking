405ba515278d8937d412b48792926fa4b6553c1d
Sveppg <terminator12327@gmail.com>
2024-06-06 00:02:35 +0200

    sorting errors

--- Daytime.c
+++ Daytime.c
@@ -1,33 +1,54 @@
-#include <complex.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <strings.h>
+#include <stdlib.h>
+#include <string.h>  // Use <string.h> instead of <strings.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-int main(int argc, char **argv){
-    int sockfd, n; 
+#define MAXLINE 4096  // Define MAXLINE with a reasonable buffer size
+
+int main(int argc, char **argv) {
+    int sockfd, n;
     char recvline[MAXLINE + 1];
     struct sockaddr_in serveraddr;
 
-    if(argc != 2){
-        perror("usage: a.out <IP-Adress>");
-        exit(EXIT_FAILURE)
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <IP-Address>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) > 0 ){
+    
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         perror("Socket error");
         exit(EXIT_FAILURE);
     }
-    bzero(&serveraddr, sizeof(serveraddr));
-    serveraddr.sin_family = AF_INET; 
+    
+    memset(&serveraddr, 0, sizeof(serveraddr));  // Use memset instead of bzero
+    serveraddr.sin_family = AF_INET;
     serveraddr.sin_port = htons(13);
-    if(inet_pton(AF_INET, argv[1], %serveraddr.sin_addr) <= 0)
-        perror("connecting error...");
-    while( (n = read(sockfd, recvline, MAXLINE))> 0){
-        recvline[n] = 0;
-        if(fputs(recvline, stdout) == EOF)  
-            perror("fputs error");
+    
+    if (inet_pton(AF_INET, argv[1], &serveraddr.sin_addr) <= 0) {
+        perror("inet_pton error");
+        exit(EXIT_FAILURE);
     }
-    if(n < 0)
-        perror("read error");
+    
+    if (connect(sockfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0) {
+        perror("Connect error");
+        exit(EXIT_FAILURE);
+    }
+    
+    while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+        recvline[n] = '\0';  // Use '\0' for null terminator
+        if (fputs(recvline, stdout) == EOF) {
+            perror("fputs error");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    if (n < 0) {
+        perror("Read error");
+        exit(EXIT_FAILURE);
+    }
 
+    close(sockfd);  // Close the socket
     return 0;
 }
